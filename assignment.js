50d8d055f4e71b485d17ee8b8173bf3087585756
Nirjhar Paul <nirjhar029@gmail.com>
2021-01-22 02:54:34 -0800

    another commit

--- assignment.js
+++ assignment.js
@@ -6,3 +6,14 @@ function kilometerToMeter(kilometer) {
 }
 
 
+var day = 12;
+var cost = 0;
+if (day <= 10) {
+    cost = day * 100
+}
+else if (day <= 20) {
+    var first10days = 10 * 100;
+    var remaining = day - 10;
+    var second10days = remaining * 80;
+    cost = first10days + second10days;
+}
