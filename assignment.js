024efe3f1c9ff602f619641dacf8bd63998dd7e7
Nirjhar Paul <nirjhar029@gmail.com>
2021-01-22 03:12:30 -0800

    second commit

--- assignment.js
+++ assignment.js
@@ -6,14 +6,26 @@ function kilometerToMeter(kilometer) {
 }
 
 
-var day = 12;
-var cost = 0;
-if (day <= 10) {
-    cost = day * 100
+function hotelCost(day) {
+    var cost = 0;
+    if (day <= 10) {
+        cost = day * 100
+    }
+    else if (day <= 20) {
+        var first10days = 10 * 100;
+        var remaining = day - 10;
+        var second10days = remaining * 80;
+        cost = first10days + second10days;
+    }
+    else {
+        var first10days = 10 * 100;
+        var second10days = 10 * 80;
+        remaining = day - 20;
+        var third10days = remaining * 50;
+        cost = first10days + second10days + third10days;
+    }
+    return cost;
 }
-else if (day <= 20) {
-    var first10days = 10 * 100;
-    var remaining = day - 10;
-    var second10days = remaining * 80;
-    cost = first10days + second10days;
-}
+
+
+
